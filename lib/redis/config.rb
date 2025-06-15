270862ab43348d5114d365bfcddc4655a4a576b2
CacoAi <cacobatph.ai@gmail.com>
2025-06-15 14:40:08 +0100

    Update config.rb

--- lib/redis/config.rb
+++ lib/redis/config.rb
@@ -11,7 +11,7 @@ module Redis::Config
 
     def base_config
       {
-        url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379'),
+        url: ENV.fetch('REDIS_URL', 'redis://default:ATN4AAIjcDEwZTA1NmJlMDExYTk0NjM3YmIxOTk1MTc4MDhhNTNhNnAxMA@robust-lab-13176.upstash.io:6379'),
         password: ENV.fetch('REDIS_PASSWORD', nil).presence,
         ssl_params: { verify_mode: Chatwoot.redis_ssl_verify_mode },
         reconnect_attempts: 2,
